59d48e286272e55d17453fc73e894a19a612c705
hamblinglen <hamblinglen@gmail.com>
2023-09-17 22:31:05 -0600

    Add project files.

---
+++ MathGame/GameEngine.cs
@@ -0,0 +1,147 @@
+﻿namespace MathGame
+{
+    internal class GameEngine
+    {
+        internal void AdditionGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Addition game selected");
+            var random = new Random();
+            int number1;
+            int number2;
+            int score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                number1 = random.Next(1, 9);
+                number2 = random.Next(1, 9);
+                Console.WriteLine($"{number1} + {number2}");
+                var answer = Console.ReadLine();
+                var result = number1 + number2;
+
+                if (int.Parse(answer) == result)
+                {
+                    Console.WriteLine("That is the correct answer!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("That is an incorrect answer!");
+                }
+                if (i == 4) break;
+                Console.WriteLine("\n");
+                Console.WriteLine("Press any key for the next question.");
+                Console.ReadLine();
+            }
+
+            Helpers.GameEnd(score, "Addition");
+        }
+        internal void SubtractionGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Subtraction game selected");
+            var random = new Random();
+            int number1;
+            int number2;
+            int score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                number1 = random.Next(1, 9);
+                number2 = random.Next(1, 9);
+                Console.WriteLine($"{number1} - {number2}");
+                var answer = Console.ReadLine();
+                var result = number1 - number2;
+
+                if (int.Parse(answer) == result)
+                {
+                    Console.WriteLine("That is the correct answer!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("That is an incorrect answer!");
+                }
+                if (i == 4) break;
+                Console.WriteLine("\n");
+                Console.WriteLine("Press any key for the next question.");
+                Console.ReadLine();
+            }
+
+            Helpers.GameEnd(score, "Subtraction");
+        }
+        internal void MultiplicationGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Multiplication game selected");
+            var random = new Random();
+            int number1;
+            int number2;
+            int score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                number1 = random.Next(1, 9);
+                number2 = random.Next(1, 9);
+                Console.WriteLine($"{number1} * {number2}");
+                var answer = Console.ReadLine();
+                var result = number1 * number2;
+
+                if (int.Parse(answer) == result)
+                {
+                    Console.WriteLine("That is the correct answer!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("That is an incorrect answer!");
+                }
+                if (i == 4) break;
+                Console.WriteLine("\n");
+                Console.WriteLine("Press any key for the next question.");
+                Console.ReadLine();
+            }
+
+            Helpers.GameEnd(score, "Multiplication");
+        }
+        internal void DivisionGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Division game selected");
+            var random = new Random();
+            int number1;
+            int number2;
+            int result;
+            int score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                number2 = random.Next(1, 10);
+                result = random.Next(1, 10);
+                number1 = number2 * result;
+                Console.WriteLine($"{number1} / {number2}");
+                var answer = Console.ReadLine();
+
+                if (int.Parse(answer) == result)
+                {
+                    Console.WriteLine("That is the correct answer!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("That is an incorrect answer!");
+                }
+                if (i == 4) break;
+                Console.WriteLine("\n");
+                Console.WriteLine("Press any key for the next question.");
+                Console.ReadLine();
+            }
+
+            Helpers.GameEnd(score, "Division");
+        }
+    }
+}
---
+++ MathGame/Helpers.cs
@@ -0,0 +1,43 @@
+﻿using MathGame.Models;
+
+namespace MathGame
+{
+    internal class Helpers
+    {
+        internal static List<Game> games = new();
+        internal static void GameEnd(int gameScore, string gameType)
+        {
+            games.Add(new Game
+            {
+                Date = DateTime.Now,
+                Score = gameScore,
+                Type = gameType
+            });
+
+            Console.WriteLine($"\nGame Over. Your score is {gameScore}");
+            Console.WriteLine("Press any key to return to the menu.");
+            Console.ReadLine();
+        }
+        internal static string GetName()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            return name;
+        }
+        internal static void PrintGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Games History");
+            Console.WriteLine("----------------------------------------");
+
+            foreach (var game in games)
+            {
+                Console.WriteLine($"{game.Date} - {game.Type}: {game.Score}pts");
+            }
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Press any button to return to the menu.");
+            Console.ReadLine();
+        }
+    }
+}
