01510c1a2add49ec94747a652f22893d32810eb7
GM-Seven <hamblinglen@gmail.com>
2023-09-18 11:38:18 -0600

    Finished

---
+++ Menu.cs
@@ -0,0 +1,60 @@
+﻿namespace MathGame
+{
+    internal class Menu
+    {
+        GameEngine engine = new();
+
+        internal void ShowMenu(string? name)
+        {
+            Console.Clear();
+            Console.WriteLine($"Hello {name}! Today is {DateTime.UtcNow.DayOfWeek}.");
+            Console.WriteLine("Press any key to show menu.");
+            Console.ReadLine();
+
+            bool isGameOn = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($@"Select one of the following games:
+v - View Game History
+a - Addition
+s - Subtraction
+m - Multiplication
+d - Division
+q - quit");
+
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        Helpers.PrintGames();
+                        break;
+                    case "a":
+                        engine.AdditionGame();
+                        break;
+                    case "s":
+                        engine.SubtractionGame();
+                        break;
+                    case "m":
+                        engine.MultiplicationGame();
+                        break;
+                    case "d":
+                        engine.DivisionGame();
+                        break;
+                    case "q":
+                        Console.WriteLine("Goodbye");
+                        isGameOn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+            while (isGameOn);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+using MathGame;
+
+Console.WriteLine("Welcom to the Math Game!");
+var name = Helpers.GetName();
+var games = new List<string>();
+var menu = new Menu();
+
+menu.ShowMenu(name);
+
+
